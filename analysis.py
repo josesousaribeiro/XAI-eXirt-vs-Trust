247f332312516d145b012b919030fc33404c08fb
José de Sousa Ribeiro Filho <jose.sousa.filho@gmail.com>
2024-06-06 16:06:55 -0300

    performances
    execucao com notacao de performances ok

---
+++ analysis.py
@@ -0,0 +1,43 @@
+import numpy
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score
+
+
+
+
+def apply_perturbation(df,percent):
+    #aplica ruido a cada instâcia do atributo da vez
+    number_of_instances = len(df.index)
+    for c in df.columns:    
+        noise = numpy.random.normal(0, percent, number_of_instances)
+        df[c] = df[c] + noise
+
+    return df
+
+def model_output_analysis(y_test,y_pred):
+      accuracy = accuracy_score(y_test, y_pred)
+      precision = precision_score(y_test, y_pred)
+      recall = recall_score(y_test, y_pred)
+      f1 = f1_score(y_test,y_pred)
+      roc_auc = roc_auc_score(y_test,y_pred)
+      return accuracy, precision, recall, f1, roc_auc
+
+
+def normalize(df):
+    # copy the dataframe
+    df_norm = df.copy()
+    # apply min-max scaling
+    for column in df_norm.columns:
+      if df[column].dtype != 'category':
+        if(len(df_norm[column].unique()) > 1): #fix NaN generation
+          df_norm[column] = (df_norm[column] - df_norm[column].min()) / (df_norm[column].max() - df_norm[column].min())
+        else:
+          df_norm[column] = 0
+    return df_norm
+
+def y_as_binary(y):
+   return  y.map({"tested_positive":1, "tested_negative":0})
--- execute_main.py
+++ execute_main.py
@@ -17,20 +17,26 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import tree
 from sklearn.neural_network import MLPClassifier
 
-#analisys imports
+#analysis imports
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedKFold
 
 
+#analysis data
+
+from analysis import * 
+import pandas as pd
 
 #initialize models
-models = {'MLP': MLPClassifier(max_iter=100),
-          'LGBM':lgb.LGBMClassifier(),
-          'KNN': KNeighborsClassifier(n_neighbors=1),
-          'DT':tree.DecisionTreeClassifier()}
+models = {
+          'mlp': MLPClassifier(verbose=False),
+          'lgbm':lgb.LGBMClassifier(verbosity=-1),
+          'knn': KNeighborsClassifier(),
+          'dt':tree.DecisionTreeClassifier()
+          }
 
-print(models)
+
 
 
 dataset = openml.datasets.get_dataset('37') #37 is dibates dataset
@@ -38,78 +44,73 @@ dataset = openml.datasets.get_dataset('37') #37 is dibates dataset
 X, Y, categorical_indicator, attribute_names = dataset.get_data(
                   dataset_format="dataframe", target=dataset.default_target_attribute)
 
+
+X = normalize(X)
+Y = y_as_binary(Y)
+
+
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, stratify=Y, random_state=42)
+
 
 #tunning models
 for key in models:
-  if key == 'MLP':
-    params_grid = {'hidden_layer_sizes': [(10,),(10,20),(10,20,30)],
-                   'activation': ['tanh', 'relu'],
-                   'solver': ['sgd', 'adam'],
-                   'alpha': [0.0001, 0.05],
-                   'learning_rate': ['constant','adaptive']}
+  if key == 'mlp':
+    params_grid = {'max_iter' : [1700],
+                   'activation' : ['identity', 'logistic', 'tanh', 'relu'],
+                   'solver' : ['sgd', 'adam'],
+                   'hidden_layer_sizes': [
+                    (3,),(4,),(5,),(6,),(7,),(8,),(9,),(10,),(11,)
+                  ]}
+  else:
+    if key == 'lgbm':
+      params_grid =  {'learning_rate': [0.1,0.5],
+                      'max_depth': [1,2,3,4],
+                      'n_estimators': [100,200,300],
+                      'min_data_in_leaf': [40, 60,80]}
+    else:
+      if key == 'knn':
+        params_grid = {'leaf_size': [1, 10, 20, 40],
+                      'algorithm': ['ball_tree', 'kd_tree', 'brute'],
+                      'metric': ['minkowski','cityblock','euclidean'],
+                      'n_neighbors': [2, 4, 6, 8, 10, 12, 14, 16]}
+      else:
+        if key == 'dt':
+          params_grid = {'min_samples_leaf': [1, 10, 20, 40],
+                        'max_depth': [1, 6, 12],
+                        'criterion': ['gini','entropy'],
+                        'splitter': ['best', 'random'],
+                        'min_samples_split': [2, 5, 15, 20, 30]}
+
+  grid_search = GridSearchCV(estimator = models[key],
+                                param_grid = params_grid,
+                                cv = StratifiedKFold(4), n_jobs = 1,
+                                verbose = 0, scoring = 'roc_auc')
+
+  # Fit the grid search to the data
+  print()
+  print('Apply gridsearch in '+key+'...')
+  grid_search.fit(X, Y) #execute the cv in all instances of data
+  models[key] = grid_search.best_estimator_
+  models[key].fit(X_train,y_train) #fit the data with correct train split
+  
+#generate perturbed datasets to test
+
+tests = {
+    'x_test_0%': X_test,
+    'x_test_15%': apply_perturbation(X_test, 0.15),
+    'x_test_30%': apply_perturbation(X_test, 0.3)}
+
+df_analysis = pd.DataFrame(index=['accuracy','precision','recall','f1','roc_auc'])
+for i in models:
+  for j in tests:
+    y_pred = models[i].predict(tests[j])
     
-    grid_search = GridSearchCV(estimator = models[key],
-                                param_grid = params_grid,
-                                cv = StratifiedKFold(4), n_jobs = 1,
-                                verbose = 0, scoring = 'roc_auc')
+    accuracy, precision, recall, f1, roc_auc = model_output_analysis(y_test,y_pred)
 
-    # Fit the grid search to the data
-    grid_search.fit(X, Y) #execute the cv in all instances of data
-    print(grid_search.best_params_)
-    models[key] = grid_search.best_estimator_
-    models[key].fit(X_train,y_train) #fit the data with correct train split
+    df_analysis.at['accuracy',i+'_'+j] = accuracy
+    df_analysis.at['precision',i+'_'+j] = precision
+    df_analysis.at['recall',i+'_'+j] = recall
+    df_analysis.at['f1',i+'_'+j] = f1
+    df_analysis.at['roc_auc',i+'_'+j] = roc_auc
 
-  if key == 'LGBM':
-    params_grid =  {'learning_rate': [0.1,0.5],
-                    'max_depth': [1,2,3,4],
-                    'n_estimators': [100,200,300],
-                    'min_data_in_leaf': [40, 60,80]}
-
-    grid_search = GridSearchCV(estimator = models[key],
-                                param_grid = params_grid,
-                                cv = StratifiedKFold(4), n_jobs = 1,
-                                verbose = 0, scoring = 'roc_auc')
-    break
-    # Fit the grid search to the data
-    grid_search.fit(X, Y) #execute the cv in all instances of data
-    print(grid_search.best_params_)
-    models[key] = grid_search.best_estimator_
-    models[key].fit(X_train,y_train) #fit the data with correct train split
-  if key == 'KNN':
-    params_grid = {'leaf_size': [1, 10, 20, 40],
-                   'algorithm': ['ball_tree', 'kd_tree', 'brute'],
-                   'metric': ['str', 'callable','minkowski'],
-                   'n_neighbors': [2, 4, 6, 8, 10, 12, 14, 16]}
-
-    grid_search = GridSearchCV(estimator = models[key],
-                                 param_grid = params_grid,
-                                 cv = StratifiedKFold(4), n_jobs = 1,
-                                 verbose = 0, scoring = 'roc_auc')
-
-    # Fit the grid search to the data
-    grid_search.fit(X, Y) #execute the cv in all instances of data
-    print(grid_search.best_params_)
-    models[key] = grid_search.best_estimator_
-    models[key].fit(X_train,y_train) #fit the data with correct train split
-  if key == 'DT':
-    params_grid = {'min_samples_leaf': [1, 10, 20, 40],
-                   'max_depth': [1, 6, 12],
-                   'criterion': ['gini','entropy'],
-                   'splitter': ['best', 'random'],
-                   'min_samples_split': [2, 5, 15, 20, 30]}
-
-    grid_search = GridSearchCV(estimator = models[key],
-                                 param_grid = params_grid,
-                                 cv = StratifiedKFold(4), n_jobs = 1,
-                                 verbose = 0, scoring = 'roc_auc')
-
-    # Fit the grid search to the data
-    grid_search.fit(X, Y) #execute the cv in all instances of data
-    print(grid_search.best_params_)
-    models[key] = grid_search.best_estimator_
-    models[key].fit(X_train,y_train) #fit the data with correct train split
-
-
-for key in models:
-  print(models[key].fit(X_train,y_train))
+print(df_analysis)
